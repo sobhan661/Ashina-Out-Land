51a44197da576a489e823a34b8a429b6d4cdb93e
sobhan661 <sobhantheimpaler@gmail.com>
2025-05-15 11:48:31 +0330

    Fix bug window not creating, change window class to Game

--- src/Game.py
+++ src/Game.py
@@ -1,12 +1,27 @@
 import sys
 import pygame
 
-class Window:
+class Game:
     """Overall class to manage game assets and behavior"""
     def __init__(self):
         """Initializes pygame and create game resources"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode(1200, 800)
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Ashina Out Land")
-        
+
+    def RunGame(self):
+        """Starts main loop for the game"""
+        while True:
+            # Watch for mouse and keyboard events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                # Make most recently drawn screen visible
+                pygame.display.flip()
+                
+if __name__ == "__main__":
+    # Make a game instance and run the game
+    game = Game()
+    game.RunGame()
